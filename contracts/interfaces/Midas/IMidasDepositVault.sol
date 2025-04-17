d903b149a2f7b8b04332cbf36d09f3ceb1c74d36
ngfam <ngfamgithub@gmail.com>
2025-04-17 10:41:36 +0700

    Added yieldtoken as tokenin & out. Added exchange rate & underlying for MidasSY

--- contracts/interfaces/Midas/IMidasDepositVault.sol
+++ contracts/interfaces/Midas/IMidasDepositVault.sol
@@ -1,9 +1,9 @@
 // SPDX-License-Identifier: MIT
 pragma solidity ^0.8.0;
 
-import "./IManageableVault.sol";
+import "./IMidasManageableVault.sol";
 
-interface IDepositVault is IManageableVault {
+interface IMidasDepositVault is IMidasManageableVault {
     function depositInstant(
         address tokenIn,
         uint256 amountToken,
