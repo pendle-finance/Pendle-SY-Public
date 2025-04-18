7aa21230da96d1d8e332ee09b62b821c3bfe230d
ngfam <ngfamgithub@gmail.com>
2025-04-18 12:50:18 +0700

    Added reservoir example

--- contracts/core/StandardizedYield/implementations/Adapter/PendleEmptyAdapter.sol
+++ contracts/core/StandardizedYield/implementations/Adapter/PendleEmptyAdapter.sol
@@ -6,29 +6,29 @@ import "../../../../interfaces/IStandardizedYieldAdapter.sol";
 contract PendleEmptyAdapter is IStandardizedYieldAdapter {
     function convertToDeposit(
         address /*tokenIn*/,
-        uint256 /*amountIn*/
+        uint256 /*amountTokenIn*/
     ) external pure override returns (address /*tokenOut*/, uint256 /*amountOut*/) {
         revert("Adapter: Not implemented");
     }
 
     function convertToRedeem(
         address /*tokenOut*/,
-        uint256 /*amountOut*/
-    ) external pure override returns (uint256 /*amountIn*/) {
+        uint256 /*amountYieldTokenIn*/
+    ) external pure override returns (uint256 /*amountOut*/) {
         revert("Adapter: Not implemented");
     }
 
     function previewConvertToDeposit(
         address /*tokenIn*/,
-        uint256 /*amountIn*/
+        uint256 /*amountTokenIn*/
     ) external pure override returns (address /*tokenOut*/, uint256 /*amountOut*/) {
         revert("Adapter: Not implemented");
     }
 
     function previewConvertToRedeem(
         address /*tokenOut*/,
-        uint256 /*amountOut*/
-    ) external pure override returns (uint256 /*amountIn*/) {
+        uint256 /*amountYieldTokenIn*/
+    ) external pure override returns (uint256 /*amountOut*/) {
         revert("Adapter: Not implemented");
     }
 
