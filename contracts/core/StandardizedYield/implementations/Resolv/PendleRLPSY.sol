f731fed9cd71e33f6e0e9923ffb9718991811242
ngfam <ngfamgithub@gmail.com>
2025-05-20 16:03:45 +0700

    Fix RLP address

--- contracts/core/StandardizedYield/implementations/Resolv/PendleRLPSY.sol
+++ contracts/core/StandardizedYield/implementations/Resolv/PendleRLPSY.sol
@@ -7,7 +7,7 @@ import "../../../../interfaces/IPExchangeRateOracle.sol";
 contract PendleRLPSY is PendleERC20SYUpg {
     using PMath for uint256;
 
-    address public constant RLP = 0xc7AB90c2Ea9271EFB31f5fA2843Eeb4B331eaFA0;
+    address public constant RLP = 0x4956b52aE2fF65D74CA2d61207523288e4528f96;
     address public constant USDC = 0xA0b86991c6218b36c1d19D4a2e9Eb0cE3606eB48;
     address public immutable exchangeRateOracle;
 
