d173b2b0a5bbdf053966ff3ba3bb53addc535b22
Nghia Pham <ngfamgithub@gmail.com>
2025-05-26 11:51:14 +0700

    Added instant redeem feature for fxsave

--- contracts/core/StandardizedYield/implementations/FX/PendleFXSaveSY.sol
+++ contracts/core/StandardizedYield/implementations/FX/PendleFXSaveSY.sol
@@ -3,8 +3,20 @@ pragma solidity ^0.8.17;
 
 import "../PendleERC4626UpgSYV2.sol";
 import "../../../../interfaces/FX/IFXBase.sol";
+import "./PendleFxCurvePoolHelper.sol";
 
-contract PendleFXSaveSY is PendleERC4626UpgSYV2 {
+interface IFxUSDBasePool {
+    function redeem(
+        address receiver,
+        uint256 amountSharesToRedeem
+    ) external returns (uint256 amountYieldOut, uint256 amountStableOut);
+
+    function previewRedeem(
+        uint256 amountSharesToRedeem
+    ) external view returns (uint256 amountYieldOut, uint256 amountStableOut);
+}
+
+contract PendleFXSaveSY is PendleERC4626UpgSYV2, PendleFxCurvePoolHelper {
     address public constant FXSAVE = 0x7743e50F534a7f9F1791DdE7dCD89F7783Eefc39;
     address public constant USDC = 0xA0b86991c6218b36c1d19D4a2e9Eb0cE3606eB48;
     address public constant FXUSD = 0x085780639CC2cACd35E474e71f4d000e2405d8f6;
@@ -28,6 +40,27 @@ contract PendleFXSaveSY is PendleERC4626UpgSYV2 {
         return super._deposit(tokenIn, amountDeposited);
     }
 
+    function _redeem(
+        address receiver,
+        address tokenOut,
+        uint256 amountSharesToRedeem
+    ) internal virtual override returns (uint256) {
+        if (tokenOut == yieldToken || tokenOut == asset) {
+            return super._redeem(receiver, tokenOut, amountSharesToRedeem);
+        } else {
+            // tokenOut is either USDC or FXUSD
+            uint256 amountAssetOut = IERC4626(yieldToken).redeem(amountSharesToRedeem, address(this), address(this));
+            (uint256 amtFxUSD, uint256 amtUSDC) = IFxUSDBasePool(asset).redeem(address(this), amountAssetOut);
+
+            uint256 amountToSwap = tokenOut == USDC ? amtFxUSD : amtUSDC;
+            uint256 amountOut = tokenOut == USDC ? amtUSDC : amtFxUSD;
+
+            amountOut += _swap(tokenOut, amountToSwap);
+            _transferOut(tokenOut, receiver, amountOut);
+            return amountOut;
+        }
+    }
+
     function _previewDeposit(
         address tokenIn,
         uint256 amountTokenToDeposit
@@ -36,6 +69,31 @@ contract PendleFXSaveSY is PendleERC4626UpgSYV2 {
             (tokenIn, amountTokenToDeposit) = (asset, IFXBase(asset).previewDeposit(tokenIn, amountTokenToDeposit));
         }
         return super._previewDeposit(tokenIn, amountTokenToDeposit);
+    }
+
+    function _previewRedeem(
+        address tokenOut,
+        uint256 amountSharesToRedeem
+    ) internal view virtual override returns (uint256 /*amountTokenOut*/) {
+        if (tokenOut == yieldToken || tokenOut == asset) {
+            return super._previewRedeem(tokenOut, amountSharesToRedeem);
+        } else {
+            // tokenOut is either USDC or FXUSD
+            uint256 amountAssetOut = IERC4626(yieldToken).previewRedeem(amountSharesToRedeem);
+            (uint256 amtFxUSD, uint256 amtUSDC) = IFxUSDBasePool(asset).previewRedeem(amountAssetOut);
+            uint256 amountToSwap = tokenOut == USDC ? amtFxUSD : amtUSDC;
+            uint256 amountOut = tokenOut == USDC ? amtUSDC : amtFxUSD;
+            amountOut += _previewSwap(tokenOut, amountToSwap);
+            return amountOut;
+        }
+    }
+
+    function getTokensOut() public view virtual override returns (address[] memory res) {
+        return ArrayLib.create(yieldToken, asset, USDC, FXUSD);
+    }
+
+    function isValidTokenOut(address token) public view virtual override returns (bool) {
+        return token == yieldToken || token == asset || token == USDC || token == FXUSD;
     }
 
     function getTokensIn() public view virtual override returns (address[] memory res) {
--- hardhat.config.ts
+++ hardhat.config.ts
@@ -26,7 +26,7 @@ const config: HardhatUserConfig = {
     solidity: {
         compilers: [
             {
-                version: '0.8.23',
+                version: '0.8.24',
                 settings: {
                     optimizer: {
                         enabled: true,
