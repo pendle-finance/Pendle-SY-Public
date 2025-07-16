6ac2c5815c777a8d3700685e3fd9c8130dd7a30b
Nghia Pham <ngfamgithub@gmail.com>
2025-07-16 10:47:27 +0700

    Move PendleHwHLPSY to Veda

--- contracts/core/StandardizedYield/implementations/Veda/PendleHwHLPSY.sol
+++ contracts/core/StandardizedYield/implementations/Veda/PendleHwHLPSY.sol
@@ -1,7 +1,7 @@
 // SPDX-License-Identifier: GPL-3.0-or-later
 pragma solidity ^0.8.23;
 
-import "./PendleVedaBaseSYV2.sol";
+import "../EtherFi/PendleVedaBaseSYV2.sol";
 import "../../../misc/TokenWithSupplyCapUpg.sol";
 
 contract PendleHwHLPSY is PendleVedaBaseSYV2, TokenWithSupplyCapUpg {
