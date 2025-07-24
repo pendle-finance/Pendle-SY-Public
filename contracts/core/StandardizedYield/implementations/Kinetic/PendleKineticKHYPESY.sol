ca3f2f984fde9a6c18f40d1bdab65923716c12b6
Nghia Pham <ngfamgithub@gmail.com>
2025-07-24 15:28:47 +0700

    Added deposit denom

--- contracts/core/StandardizedYield/implementations/Kinetic/PendleKineticKHYPESY.sol
+++ contracts/core/StandardizedYield/implementations/Kinetic/PendleKineticKHYPESY.sol
@@ -5,12 +5,13 @@ import "../../v2/SYBaseUpgV2.sol";
 import "../../../../interfaces/Kinetic/IKineticStakingAccountant.sol";
 import "../../../../interfaces/Kinetic/IKineticStakingManager.sol";
 
-contract PendleKineticKHypeSY is SYBaseUpgV2 {
+contract PendleKineticKHYPESY is SYBaseUpgV2 {
     using PMath for uint256;
 
     address public constant KHYPE = 0xfD739d4e423301CE9385c1fb8850539D657C296D;
     address public constant STAKING_MANAGER = 0x393D0B87Ed38fc779FD9611144aE649BA6082109;
     address public constant STAKING_ACCOUNTANT = 0x9209648Ec9D448EF57116B73A2f081835643dc7A;
+    uint256 public constant DEPOSIT_DENOM = 1e10;
 
     constructor() SYBaseUpgV2(KHYPE) {}
 
@@ -27,7 +28,7 @@ contract PendleKineticKHypeSY is SYBaseUpgV2 {
         }
 
         uint256 preBalance = _selfBalance(yieldToken);
-        IKineticStakingManager(STAKING_MANAGER).stake{value: amountDeposited}();
+        IKineticStakingManager(STAKING_MANAGER).stake{value: _truncAmount(amountDeposited)}();
         return _selfBalance(yieldToken) - preBalance;
     }
 
@@ -84,5 +85,9 @@ contract PendleKineticKHypeSY is SYBaseUpgV2 {
         returns (AssetType assetType, address assetAddress, uint8 assetDecimals)
     {
         return (AssetType.TOKEN, NATIVE, 18);
+    }
+
+    function _truncAmount(uint256 amount) internal pure virtual returns (uint256) {
+        return amount - (amount % DEPOSIT_DENOM);
     }
 }
